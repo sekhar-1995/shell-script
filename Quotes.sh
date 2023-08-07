c1311158e445f6dc0170b5112e6130f4567996f4
sekhar-1995 <119853679+sekhar-1995@users.noreply.github.com>
2023-08-08 00:22:29 +0530

    Update Quotes.sh

--- Quotes.sh
+++ Quotes.sh
@@ -14,5 +14,7 @@ Due to covid-19 company lost $9 million.
 
 # so when a value is written inside "single Quote" i.e [''] for varible, then it will reasign/replace the value of the variable.
 # But in the case of {''}, it removes the value of th especial charater like [$].
+
+cosider the case of #3
 # for Ex:- the "#3", here we want to print the value of word followed by "$", but for $9 there is no value is assigned. 
 --> In this case to print the value we will use "\" that will ignore thw special character i.e "$" & print the value
