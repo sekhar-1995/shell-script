12bd7c1c594b66ebd629da055a27b9c483f15b3e
sekhar-1995 <subham742rath@gmail.com>
2023-08-07 20:32:30 +0530

    adding firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Here "#!" The shebang, or hash-bang character
+
+###To see thedetails of our device###
+
+####################
+### Welcome letter
+echo "Welcome to shellscript"
+echo
+
+echo "##########################"
+###To check the Uptime ###
+echo "Uptime of the system"
+uptime
+echo
+
+echo "##################"
+### To check Memory Utilization ###
+echo "Memory utilization"
+free -m
+echo
+
+echo "###################"
+### To check Disc Utilization ###
+echo "disc utilization"
+df -h
